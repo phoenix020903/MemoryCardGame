899b5625ff724cb5bb7d850394c39e52e97b2bba
phoenix020903 <129112358+phoenix020903@users.noreply.github.com>
2024-02-09 20:27:56 +0530

    Add files via upload

---
+++ cat.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s1,s2;
+        for(int i=0;i<n;i++){
+            cin>>s1;
+        }
+        for(int i=0;i<n;i++){
+            cin>>s2;
+        }
+    }
+}
